29ea85167ffb950478f9563f1826a2dd594b6b8c
BT <btinsley@usc.edu>
2024-03-22 16:08:01 -0700

    light mode

--- pages/2_Faculty_Publications.py
+++ pages/2_Faculty_Publications.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 st.set_page_config(layout="wide", page_title="Price Data Project", page_icon = "✌")
 
-st.markdown("""
-    <style>
-    #MainMenu {display: none;}
-    .stDeployButton {display: none;}
-    [data-testid="stHeader"] {background-color: #222222;}
-    .main.st-emotion-cache-uf99v8.ea3mdgi8 {background-color: #222222;}
-    .st-emotion-cache-1dj0hjr{color: #FFC72C;}
-    .st-emotion-cache-1m6wrpk.eczjsme5 {color: #fff;}
-    .st-emotion-cache-6qob1r.eczjsme3 {background-color: #990000;}
-    </style>
-    """, unsafe_allow_html=True)
+# st.markdown("""
+#     <style>
+#     #MainMenu {display: none;}
+#     .stDeployButton {display: none;}
+#     [data-testid="stHeader"] {background-color: #222222;}
+#     .main.st-emotion-cache-uf99v8.ea3mdgi8 {background-color: #222222;}
+#     .st-emotion-cache-1dj0hjr{color: #FFC72C;}
+#     .st-emotion-cache-1m6wrpk.eczjsme5 {color: #fff;}
+#     .st-emotion-cache-6qob1r.eczjsme3 {background-color: #990000;}
+#     </style>
+#     """, unsafe_allow_html=True)
 
 st.header('USC Price Faculty Publications', divider='red')
 st.subheader('')
